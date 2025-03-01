16ae215bd73c57613929bd08653f2324f6453ac5
SehajBindra <sehajbindra1234@gmail.com>
2025-03-01 20:01:51 +0530

    fixed sidebar close issue when clicking on an item

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/public/TVSLogo-hr.svg";
-import { Menu } from "lucide-react";
+import { Menu, PanelLeftOpen } from "lucide-react";
 
 import {
   NavigationMenu,
@@ -16,7 +16,7 @@ import {
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
 import { useState } from "react";
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
 import ProductMenu from "./ProductMenu";
 
@@ -40,70 +40,73 @@ export default function Navbar() {
     return (
       <>
         {/* Side Menu */}
-        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <Sheet >
           <SheetTrigger asChild>
             <Button
               variant="ghost"
-              size="default"
-              className="lg:hidden hover:bg-transparent"
+              size="icon"
+              className="lg:hidden w-8 h-8"
               aria-label="Toggle Menu"
             >
-              <Menu />
+              <PanelLeftOpen className="w-8 h-8" />
             </Button>
           </SheetTrigger>
-          <SheetContent side="left" className="w-[400px] sm:w-[300px]">
-            <div className="flex flex-col space-y-4 mt-8">
-              <div className="space-y-4 py-4">
-                <div className="px-2 py-1">
-                  <h2 className="text-lg font-semibold">Products</h2>
-                  <ProductMenu />
-                </div>
-                <div className="px-2 py-1">
-                  <Link
-                    href="/services"
-                    className="block font-medium text-customBlue"
-                    onClick={() => setIsOpen(false)}
-                  >
-                    Services
-                  </Link>
-                </div>
-                <div className="px-2 py-1">
-                  <h2 className="mb-2 font-medium text-customBlue">Shop</h2>
-                  <div className="space-y-2">
-                    {/* Add your shop links here */}
-                    <p className="text-sm">Shop content goes here</p>
+          <SheetContent side="left" className="w-[400px] overflow-y-auto sm:w-[300px]">
+
+            <SheetClose asChild>
+              <div className="flex flex-col space-y-4 mt-8">
+                <div className="space-y-4 py-4">
+                  <div className="px-2 py-1">
+                    <h2 className="text-lg font-semibold">Products</h2>
+                    <ProductMenu />
                   </div>
-                </div>
-                <div className="px-2 py-1">
-                  <h2 className="mb-2 font-medium text-customBlue">Company</h2>
-                  <div className="space-y-2">
-                    {/* Add your company links here */}
-                    <p className="text-sm">Company content goes here</p>
+                  <div className="px-2 py-1">
+                    <Link
+                      href="/services"
+                      className="block font-medium text-customBlue"
+                      onClick={() => setIsOpen(false)}
+                    >
+                      Services
+                    </Link>
                   </div>
-                </div>
-                {/* Secondary Links */}
-                <div className="w-full flex flex-col gap-4 items-start justify-start mt-5 px-2">
-                  {/* <Link
+                  <div className="px-2 py-1">
+                    <h2 className="mb-2 font-medium text-customBlue">Shop</h2>
+                    <div className="space-y-2">
+                      {/* Add your shop links here */}
+                      <p className="text-sm">Shop content goes here</p>
+                    </div>
+                  </div>
+                  <div className="px-2 py-1">
+                    <h2 className="mb-2 font-medium text-customBlue">Company</h2>
+                    <div className="space-y-2">
+                      {/* Add your company links here */}
+                      <p className="text-sm">Company content goes here</p>
+                    </div>
+                  </div>
+                  {/* Secondary Links */}
+                  <div className="w-full flex flex-col gap-4 items-start justify-start mt-5 px-2">
+                    {/* <Link
                     href="/buy-vehicle"
                     className="flex flex-col items-start space-y-1"
                   >
                     <span className="text-gray-400">Buy Vehicle</span>
                   </Link> */}
-                  <Link
-                    href="/test-ride"
-                    className="px-4 py-2 text-sm font-medium text-white bg-customBlue rounded-md hover:bg-blue-600 transition-colors"
-                  >
-                    Book A Test Ride
-                  </Link>
-                  {/* <Link
+                    <Link
+                      href="/test-ride"
+                      className="px-4 py-2 text-sm font-medium text-white bg-customBlue rounded-md hover:bg-blue-600 transition-colors"
+                    >
+                      Book A Test Ride
+                    </Link>
+                    {/* <Link
                     href="/dealers"
                     className="flex flex-col items-start space-y-1"
                   >
                     <span className="text-gray-400">Dealers</span>
                   </Link> */}
+                  </div>
                 </div>
               </div>
-            </div>
+            </SheetClose>
           </SheetContent>
         </Sheet>
       </>
