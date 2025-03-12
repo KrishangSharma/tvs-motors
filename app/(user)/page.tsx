0f1611724f8c13f4718f0a57567424db1381ce37
Krishang Sharma <krishang.sharma.17704@gmail.com>
2025-03-12 23:20:20 +0530

    test

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,16 +1,14 @@
 "use client";
 
 import { Hero, Stories, YourRide } from "@/components/exports";
-import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+// import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 export default function Home() {
   return (
-    <GoogleReCaptchaProvider reCaptchaKey="6LclavIqAAAAAL48sDjZDsdPk-Q9pxUXSD1Q0qAc">
-      <main className="w-full flex flex-col gap-5">
-        <Hero />
-        <Stories />
-        <YourRide />
-      </main>
-    </GoogleReCaptchaProvider>
+    <main className="w-full flex flex-col gap-5">
+      <Hero />
+      <Stories />
+      <YourRide />
+    </main>
   );
 }
